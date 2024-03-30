317d45b8b496629d3bb1b6a3f728ac7c8f3b872e
Fauzan Akmal Mahdi <recovery252@gmail.com>
2024-03-30 12:24:30 +0700

    fix loading still show after navigate ktp ocr screen

--- kebut_kurir/lib/features/ktp_ocr_result/presentation/ktp_ocr_result_controller.dart
+++ kebut_kurir/lib/features/ktp_ocr_result/presentation/ktp_ocr_result_controller.dart
@@ -89,7 +89,19 @@ class KTPOcrResultController extends GetxController {
   String normalizeLahirText(String text) {
     String result = text.toUpperCase();
 
-    result = result.replaceAll('GLAHIR', '').replaceAll('LAHIR', '').replaceAll('/TGL', '').replaceAll('/TL', '').replaceAll('TL', '').replaceAll('TMPT', '').replaceAll('TPT', '').replaceAll('TEMPAT', '').replaceAll(',', '').replaceAll(' :', '').replaceAll(': ', '').trim();
+    result = result
+        .replaceAll('GLAHIR', '')
+        .replaceAll('LAHIR', '')
+        .replaceAll('/TGL', '')
+        .replaceAll('/TL', '')
+        .replaceAll('TL', '')
+        .replaceAll('TMPT', '')
+        .replaceAll('TPT', '')
+        .replaceAll('TEMPAT', '')
+        .replaceAll(',', '')
+        .replaceAll(' :', '')
+        .replaceAll(': ', '')
+        .trim();
 
     return result;
   }
@@ -97,12 +109,78 @@ class KTPOcrResultController extends GetxController {
   String normalizeJenisKelaminText(String text) {
     String result = text.toUpperCase();
 
-    result = result.replaceAll('GOL. DARAHO', '').replaceAll('GOL. DARAH', '').replaceAll('GOL DARAH', '').replaceAll('LAKFEARI', '').replaceAll('LAKFLAK', '').replaceAll('KELAMIN', '').replaceAll('KEIAMIN', '').replaceAll('JENIS', '').replaceAll('DENIS', '').replaceAll('DARAH ', '').replaceAll('ENIS', '').replaceAll('DARA', '').replaceAll('GO', '').replaceAll('L. ', '').replaceAll(' H0', '').replaceAll(' HO', '').replaceAll(':', '').replaceAll(' :', '').replaceAll(' 0', '').replaceAll(' O', '').trim();
+    result = result
+        .replaceAll('GOL. DARAHO', '')
+        .replaceAll('GOL. DARAH', '')
+        .replaceAll('GOL DARAH', '')
+        .replaceAll('LAKFEARI', '')
+        .replaceAll('LAKFLAK', '')
+        .replaceAll('KELAMIN', '')
+        .replaceAll('KEIAMIN', '')
+        .replaceAll('JENIS', '')
+        .replaceAll('DENIS', '')
+        .replaceAll('DARAH ', '')
+        .replaceAll('ENIS', '')
+        .replaceAll('DARA', '')
+        .replaceAll('GO', '')
+        .replaceAll('L. ', '')
+        .replaceAll(' H0', '')
+        .replaceAll(' HO', '')
+        .replaceAll(':', '')
+        .replaceAll(' :', '')
+        .replaceAll(' 0', '')
+        .replaceAll(' O', '')
+        .trim();
 
-    if (result.contains('LAK-LAK') || result == 'LAKI-LAK' || result == 'AK-LAK' || result == 'LAKFLAKI' || result == 'LAKHLAK' || result == 'LAKFEAKI' || result == 'LAKELAKI' || result == 'LAKELAK' || result == 'LAKHLAKI' || result == 'LAKHEAK' || result == 'LAK-LAKI' || result == 'LAKHEAKI' || result == 'LAKIFEAK' || result == 'LAKFEAKE' || result == 'LAKIFEAKI' || result == 'LAKFEAR' || result == 'LAKFLAK' || result == 'LAK-LAKE' || result == 'LAK-EAK' || result.contains('LA') || result == 'LAK-EAKI' || result.contains('L') || result == 'LAKELAKE') {
+    if (result.contains('LAK-LAK') ||
+        result == 'LAKI-LAK' ||
+        result == 'AK-LAK' ||
+        result == 'LAKFLAKI' ||
+        result == 'LAKHLAK' ||
+        result == 'LAKFEAKI' ||
+        result == 'LAKELAKI' ||
+        result == 'LAKELAK' ||
+        result == 'LAKHLAKI' ||
+        result == 'LAKHEAK' ||
+        result == 'LAK-LAKI' ||
+        result == 'LAKHEAKI' ||
+        result == 'LAKIFEAK' ||
+        result == 'LAKFEAKE' ||
+        result == 'LAKIFEAKI' ||
+        result == 'LAKFEAR' ||
+        result == 'LAKFLAK' ||
+        result == 'LAK-LAKE' ||
+        result == 'LAK-EAK' ||
+        result.contains('LA') ||
+        result == 'LAK-EAKI' ||
+        result.contains('L') ||
+        result == 'LAKELAKE') {
       return 'Laki-Laki';
     }
-    if (result == 'PEREM' || result == 'PUAN' || result == 'PER' || result == 'PERU' || result == 'PU' || result == 'PERI' || result == 'PERA' || result == 'RE' || result == 'PEREH' || result == 'PRH' || result == 'PRHA' || result == 'PHUAN' || result == 'EREM' || result == 'REHN' || result == 'MPUAN' || result.contains('P') || result.contains('PE') || result == 'PEA' || result == 'PERHUAN' || result == 'PRUMPAN' || result == 'EMPUAN' || result == 'RUMPUAN' || result == 'HAMPUAN' || result == 'REMPUN') {
+    if (result == 'PEREM' ||
+        result == 'PUAN' ||
+        result == 'PER' ||
+        result == 'PERU' ||
+        result == 'PU' ||
+        result == 'PERI' ||
+        result == 'PERA' ||
+        result == 'RE' ||
+        result == 'PEREH' ||
+        result == 'PRH' ||
+        result == 'PRHA' ||
+        result == 'PHUAN' ||
+        result == 'EREM' ||
+        result == 'REHN' ||
+        result == 'MPUAN' ||
+        result.contains('P') ||
+        result.contains('PE') ||
+        result == 'PEA' ||
+        result == 'PERHUAN' ||
+        result == 'PRUMPAN' ||
+        result == 'EMPUAN' ||
+        result == 'RUMPUAN' ||
+        result == 'HAMPUAN' ||
+        result == 'REMPUN') {
       return 'Perempuan';
     }
 
@@ -112,7 +190,62 @@ class KTPOcrResultController extends GetxController {
   String normalizeAlamatText(String text) {
     String result = text.toUpperCase();
 
-    result = result.replaceAll('RI/KEILDESAA', '').replaceAll('RTKELIIDESAA', '').replaceAll('TIKEL/LDESA', '').replaceAll('RTKEL/DESAA', '').replaceAll('RTKELVDESA', '').replaceAll('RIKELBESAA', '').replaceAll('KECAMATAN', '').replaceAll('KEL/DESSA', '').replaceAll('KELIDESAA', '').replaceAll('KELI/DESA', '').replaceAll('KELILDESA', '').replaceAll('KELIIDESA', '').replaceAll('KELILDESA', '').replaceAll('KEL/ DESA', '').replaceAll('KELLIDESA', '').replaceAll('KECAMATDN', '').replaceAll('HECAMATAN', '').replaceAll('KEILIBESA', '').replaceAll('KELILBESA', '').replaceAll('NECAMATAN', '').replaceAll('KELL/DESA', '').replaceAll('KEL/DESAA', '').replaceAll('KELLDESAA', '').replaceAll('KEL/DESA', '').replaceAll('KELLIBES', '').replaceAll('KEI/DESA', '').replaceAll('HELLDESA', '').replaceAll('KELIBESA', '').replaceAll('KELLBESA', '').replaceAll('KEL/DESA', '').replaceAll('KELLDESA', '').replaceAll('KEILDESA', '').replaceAll('KEILBESA', '').replaceAll('KELIDESA', '').replaceAll('KEVDESA', '').replaceAll('KEVBESA', '').replaceAll('KELBESA', '').replaceAll('KE/DESA', '').replaceAll('ELLDESA', '').replaceAll('KELDESA', '').replaceAll('ALAMAT', '').replaceAll('LAMAT', '').replaceAll('RTIRW', '').replaceAll('RT/RW', '').replaceAll('ELDESA', '').replaceAll('KEVDES', '').replaceAll('RTIRWN', '').replaceAll(' TIA ', ' ').replaceAll('RT ', '').replaceAll('RT/ ', '').replaceAll('RW ', '').replaceAll(':', '').replaceAll('=', '').replaceAll('  ', ' ').trim();
+    result = result
+        .replaceAll('RI/KEILDESAA', '')
+        .replaceAll('RTKELIIDESAA', '')
+        .replaceAll('TIKEL/LDESA', '')
+        .replaceAll('RTKEL/DESAA', '')
+        .replaceAll('RTKELVDESA', '')
+        .replaceAll('RIKELBESAA', '')
+        .replaceAll('KECAMATAN', '')
+        .replaceAll('KEL/DESSA', '')
+        .replaceAll('KELIDESAA', '')
+        .replaceAll('KELI/DESA', '')
+        .replaceAll('KELILDESA', '')
+        .replaceAll('KELIIDESA', '')
+        .replaceAll('KELILDESA', '')
+        .replaceAll('KEL/ DESA', '')
+        .replaceAll('KELLIDESA', '')
+        .replaceAll('KECAMATDN', '')
+        .replaceAll('HECAMATAN', '')
+        .replaceAll('KEILIBESA', '')
+        .replaceAll('KELILBESA', '')
+        .replaceAll('NECAMATAN', '')
+        .replaceAll('KELL/DESA', '')
+        .replaceAll('KEL/DESAA', '')
+        .replaceAll('KELLDESAA', '')
+        .replaceAll('KEL/DESA', '')
+        .replaceAll('KELLIBES', '')
+        .replaceAll('KEI/DESA', '')
+        .replaceAll('HELLDESA', '')
+        .replaceAll('KELIBESA', '')
+        .replaceAll('KELLBESA', '')
+        .replaceAll('KEL/DESA', '')
+        .replaceAll('KELLDESA', '')
+        .replaceAll('KEILDESA', '')
+        .replaceAll('KEILBESA', '')
+        .replaceAll('KELIDESA', '')
+        .replaceAll('KEVDESA', '')
+        .replaceAll('KEVBESA', '')
+        .replaceAll('KELBESA', '')
+        .replaceAll('KE/DESA', '')
+        .replaceAll('ELLDESA', '')
+        .replaceAll('KELDESA', '')
+        .replaceAll('ALAMAT', '')
+        .replaceAll('LAMAT', '')
+        .replaceAll('RTIRW', '')
+        .replaceAll('RT/RW', '')
+        .replaceAll('ELDESA', '')
+        .replaceAll('KEVDES', '')
+        .replaceAll('RTIRWN', '')
+        .replaceAll(' TIA ', ' ')
+        .replaceAll('RT ', '')
+        .replaceAll('RT/ ', '')
+        .replaceAll('RW ', '')
+        .replaceAll(':', '')
+        .replaceAll('=', '')
+        .replaceAll('  ', ' ')
+        .trim();
 
     print('result result result result result ' + result);
 
@@ -122,7 +255,19 @@ class KTPOcrResultController extends GetxController {
   String normalizeKawinText(String text) {
     String result = text.toUpperCase();
 
-    result = result.replaceAll('PERKAWINAN', '').replaceAll('PERKAWINA', '').replaceAll('STATUS', '').replaceAll('TATUS', '').replaceAll('Perkawinar', '').replaceAll('Status Perkawinar', '').replaceAll('STAFUS', '').replaceAll('R ', '').replaceAll('R', '').replaceAll('T ', '').replaceAll(':', '').trim();
+    result = result
+        .replaceAll('PERKAWINAN', '')
+        .replaceAll('PERKAWINA', '')
+        .replaceAll('STATUS', '')
+        .replaceAll('TATUS', '')
+        .replaceAll('Perkawinar', '')
+        .replaceAll('Status Perkawinar', '')
+        .replaceAll('STAFUS', '')
+        .replaceAll('R ', '')
+        .replaceAll('R', '')
+        .replaceAll('T ', '')
+        .replaceAll(':', '')
+        .trim();
 
     return result;
   }
@@ -151,7 +296,23 @@ class KTPOcrResultController extends GetxController {
 
     result = result.replaceAll('AGAMA', '').replaceAll(':', '').replaceAll('GAMA', '').trim();
 
-    if (result == 'SLAM' || result == 'AM' || result == 'SLA AM' || result == 'ISLU AM' || result == 'SL LAM' || result == 'ISLAME' || result == 'SLA M' || result == 'ISL AM' || result == 'ISLA AM' || result == 'S AM' || result == 'SLL AM' || result == 'SL AM' || result == 'SE AM' || result == '1SLAM' || result == 'ISLAMM' || result == 'SLA' || result == 'LAM') {
+    if (result == 'SLAM' ||
+        result == 'AM' ||
+        result == 'SLA AM' ||
+        result == 'ISLU AM' ||
+        result == 'SL LAM' ||
+        result == 'ISLAME' ||
+        result == 'SLA M' ||
+        result == 'ISL AM' ||
+        result == 'ISLA AM' ||
+        result == 'S AM' ||
+        result == 'SLL AM' ||
+        result == 'SL AM' ||
+        result == 'SE AM' ||
+        result == '1SLAM' ||
+        result == 'ISLAMM' ||
+        result == 'SLA' ||
+        result == 'LAM') {
       result = 'Islam';
     }
     print(result);
@@ -532,34 +693,38 @@ class KTPOcrResultController extends GetxController {
   }
 
   void onPressToNextStep() async {
-    dialogsUtils.showLoading();
-    await recognizedText(ocr.value!.path);
-    if (ocr.value != null && ocrCropped.value != null) {
-      Get.toNamed(
-        Routes.ocrKtpConfirm,
-        arguments: KtpConfirmDataArgs(
-          card: card,
-          ocr: ocr.value!,
-          ocrCropped: ocrCropped.value!,
-          agama: agama,
-          jenisKelamin: jenisKelamin,
-          rw: rtrw,
-          alamat: alamatFull,
-          kecamatan: kecamatan,
-          kelurahan: kelDesa,
-          kewarganegaraan: kewarganegaraan,
-          rt: rtrw,
-          statusKawin: statusKawin,
-          tempatLahir: tempatLahir,
-          tglLahir: tglLahir,
-          nama: name,
-          nik: nik,
-        ),
-      );
-    } else {
-      dialogsUtils.showError('Terjadi Kesalahan');
+    try {
+      dialogsUtils.showLoading();
+      await recognizedText(ocr.value!.path);
+      if (ocr.value != null && ocrCropped.value != null) {
+        Get.toNamed(
+          Routes.ocrKtpConfirm,
+          arguments: KtpConfirmDataArgs(
+            card: card,
+            ocr: ocr.value!,
+            ocrCropped: ocrCropped.value!,
+            agama: agama,
+            jenisKelamin: jenisKelamin,
+            rw: rtrw,
+            alamat: alamatFull,
+            kecamatan: kecamatan,
+            kelurahan: kelDesa,
+            kewarganegaraan: kewarganegaraan,
+            rt: rtrw,
+            statusKawin: statusKawin,
+            tempatLahir: tempatLahir,
+            tglLahir: tglLahir,
+            nama: name,
+            nik: nik,
+          ),
+        )?.then((value) => dialogsUtils.hideLoading());
+      } else {
+        dialogsUtils.showError('Terjadi Kesalahan');
+      }
+      print('ISI HASIL EXTRAK $extractedText');
+    } catch (e) {
+      dialogsUtils.hideLoading();
     }
-    print('ISI HASIL EXTRAK $extractedText');
   }
 
   @override
